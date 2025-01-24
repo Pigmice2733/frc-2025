a3a222ad65cc9492db61db9a965c5cec59823353
Alex <avhoi83@gmail.com>
2025-01-23 16:01:39 -0800

    removed unnecessary sources in template

--- src/main/java/frc/robot/subsystems/TemplateSubsystem.java
+++ src/main/java/frc/robot/subsystems/TemplateSubsystem.java
@@ -1,40 +1,17 @@
 package frc.robot.subsystems;
 
-import java.io.File;
-import java.io.IOException;
-
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.config.RobotConfig;
-import com.pathplanner.lib.controllers.PPHolonomicDriveController;
-import com.pathplanner.lib.util.PathPlannerLogging;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.*;
-import swervelib.SwerveDrive;
-import swervelib.SwerveModule;
-import swervelib.imu.SwerveIMU;
-import swervelib.parser.SwerveParser;
 
 public class TemplateSubsystem extends SubsystemBase {
-  private CANSparkMax motor = new CANSparkMax(MotorType.kBrushless, 0);
+  private SparkMax motor = new SparkMax(MotorType.kBrushless, 0);
 
   private ShuffleboardTab tab = ShuffleboardConfig.DRIVETRAIN_TAB;
   private ShuffleboardLayout entryList;
